30deb3b00aeca65a74d25a4bbcfe221b3a87fee7
ilaidris2010 <fauzilah.idris1@gmail.com>
2025-10-16 12:56:49 +0800

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\BookCategoryController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+   return view('hello');
+});
+
+Route::get('/hello1', function () {
+  return view('hello1', ['user' => 'John']);
+});
+
+Route::get('/hello2/{user}', function ($user) {
+  return view('hello2', ['user' => $user]);
+});
+
+Route::resource('books', BookController::class);
+
+Route::resource('bookcategories', BookCategoryController::class);
+
+
+
+
+
